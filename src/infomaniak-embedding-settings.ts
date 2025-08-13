14b809c66b204149aae6eb1793d0c1a0d568abb3
Stephan Simonett <wireless25@me.com>
2025-08-13 20:58:27 +0200

    types: remove obsolete type files

--- src/infomaniak-embedding-settings.ts
+++
@@ -1,1 +0,0 @@
-export type InfomaniakEmbeddingModelIds = 'mini_lm_l12_v2' | 'bge_multilingual_gemma2' | (string & {})
--- src/infomaniak-image-settings.ts
+++
@@ -1,1 +0,0 @@
-export type InfomaniakImageModelIds = 'flux' | 'photomaker' | (string & {})
